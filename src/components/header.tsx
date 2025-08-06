6d901c5c37a52b7d5c1b89852ecb8d8a491ba74d
Richard Snider <lighthousemaniac@gmail.com>
2025-08-05 19:16:23 -0600

    add even more *style* to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/Header.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 type NavbarEntry = { text: string; link: string };
 
@@ -15,18 +16,35 @@ const NAVBAR_ENTRIES: NavbarEntry[] = [
 ];
 
 export default function Header() {
+  const [hidden, setHidden] = useState(true);
+
+  function toggleNavbar() {
+    setHidden(!hidden);
+  }
+
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>guest@usufslc %</div>
-      <ul className={styles.links}>
-        {NAVBAR_ENTRIES.map((entry) => {
-          return (
-            <li key={entry.text}>
-              [&nbsp;<Link href={entry.link}>{entry.text}</Link>&nbsp;]
-            </li>
-          );
-        })}
-      </ul>
+    <nav className={`${styles.navbar} ${hidden ? styles.hidden : ""}`}>
+      <div className={styles.left}>
+        <div className={styles.logo}>guest@usufslc %</div>
+        <ul className={styles.links}>
+          {NAVBAR_ENTRIES.map((entry) => {
+            return (
+              <li key={entry.text}>
+                [&nbsp;<Link href={entry.link}>{entry.text}</Link>&nbsp;]
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className={styles.right}>
+        <span className={styles.expander}>
+          [&nbsp;
+          <button type="button" onClick={toggleNavbar} className="link">
+            {hidden ? "++" : "--"}
+          </button>
+          &nbsp;]
+        </span>
+      </div>
     </nav>
   );
 }
