9b4fba29803a2a80324d14f30effc22c193e4bf0
Richard Snider <lighthousemaniac@gmail.com>
2025-11-15 23:29:12 -0700

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <>
+      Copyright &copy; USU FSLC Members
+      <br />
+      This program is free software under the{" "}
+      <a href="https://gnu.org/licenses/">GPL v3+</a>.
+      <br />
+      <a href="https://github.com/USUFSLC/website-frontend-2024">
+        View or contribute to the source code
+      </a>
+      .
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import "@/styles/globals.css";
 import "@/styles/calendar.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Footer from "@/components/footer.tsx";
 
 type StaticProps = {
   idToken: string | null;
@@ -18,10 +19,15 @@ export default function MyApp({ Component, pageProps }: AppProps<StaticProps>) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <AuthProvider initialAuthSession={pageProps.initialAuthSession}>
-        <header>
-          <Header />
-        </header>
-        <Component {...pageProps} />
+        <div id="page-container">
+          <header>
+            <Header />
+          </header>
+          <Component {...pageProps} />
+          <footer>
+            <Footer />
+          </footer>
+        </div>
       </AuthProvider>
     </>
   );
